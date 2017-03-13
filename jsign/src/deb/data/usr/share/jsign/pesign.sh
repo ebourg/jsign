5d00b9ff973f44928a935bff89cce700c6ca4ca8
Emmanuel Bourg <ebourg@apache.org>
2017-03-14 00:58:37 +0100

    Replace the version in the shell script and the man page (Fixes #37)

--- jsign/src/deb/data/usr/share/jsign/pesign.sh
+++ jsign/src/deb/data/usr/share/jsign/pesign.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xmx128m -Djava.net.useSystemProxies=true -jar /usr/share/jsign/jsign-1.2.jar "$@"
+java -Xmx128m -Djava.net.useSystemProxies=true -jar /usr/share/jsign/jsign-@VERSION@.jar "$@"
