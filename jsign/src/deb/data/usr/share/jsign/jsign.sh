19bc975146508c6ca673502c99e71ea6ac51fe29
Emmanuel Bourg <ebourg@apache.org>
2021-03-26 08:42:37 +0100

    Removed the maximum heap size in the command line script (#82)

--- jsign/src/deb/data/usr/share/jsign/jsign.sh
+++ jsign/src/deb/data/usr/share/jsign/jsign.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xmx128m -Djava.net.useSystemProxies=true -Dbasename=`basename "$0"` -jar /usr/share/jsign/jsign-@VERSION@.jar "$@"
+java -Djava.net.useSystemProxies=true -Dbasename=`basename "$0"` -jar /usr/share/jsign/jsign-@VERSION@.jar "$@"
