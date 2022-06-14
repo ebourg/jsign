722a53fa80ec5e4256e88455d90d54935d2a7c9c
Emmanuel Bourg <ebourg@apache.org>
2022-06-14 17:54:43 +0200

    Added an example showing how to use the Gradle plugin with the Kotlin DSL (Fixes #72)

---
+++ jsign-gradle-plugin/example.gradle.kts
@@ -0,0 +1,24 @@
+buildscript {
+    repositories {
+        mavenLocal()
+    }
+
+    dependencies {
+        classpath("net.jsign:jsign-gradle-plugin:4.1")
+    }
+}
+
+apply(plugin = "net.jsign")
+
+task("sign") {
+    doLast {
+        val jsign = project.extensions.getByName("jsign") as groovy.lang.Closure<*>
+        jsign("file"      to "application.exe",
+              "name"      to "My Application",
+              "url"       to "http://www.example.com",
+              "keystore"  to "keystore.p12",
+              "alias"     to "test",
+              "storepass" to "secret",
+              "tsaurl"    to "http://timestamp.sectigo.com")
+    }
+}
