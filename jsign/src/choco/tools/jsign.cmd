59d0e01b316c4238d2d0e782b7315eef0f742f36
Emmanuel Bourg <ebourg@apache.org>
2023-09-12 15:14:27 +0200

    Line breaks in the launcher scripts

--- jsign/src/choco/tools/jsign.cmd
+++ jsign/src/choco/tools/jsign.cmd
@@ -1,3 +1,6 @@
 @echo off
 
-java -Djava.net.useSystemProxies=true -Dbasename=jsign -Dlog4j2.loggerContextFactory=net.jsign.log4j.simple.SimpleLoggerContextFactory -jar %~dp0\jsign.jar %*
+java -Djava.net.useSystemProxies=true ^
+     -Dbasename=jsign ^
+     -Dlog4j2.loggerContextFactory=net.jsign.log4j.simple.SimpleLoggerContextFactory ^
+     -jar %~dp0\jsign.jar %*
--- jsign/src/deb/data/usr/share/jsign/jsign.sh
+++ jsign/src/deb/data/usr/share/jsign/jsign.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-java -Djava.net.useSystemProxies=true -Dbasename=`basename "$0"` -Dlog4j2.loggerContextFactory=net.jsign.log4j.simple.SimpleLoggerContextFactory -jar /usr/share/jsign/jsign-@VERSION@.jar "$@"
+java -Djava.net.useSystemProxies=true \
+     -Dbasename=`basename "$0"` \
+     -Dlog4j2.loggerContextFactory=net.jsign.log4j.simple.SimpleLoggerContextFactory \
+     -jar /usr/share/jsign/jsign-@VERSION@.jar "$@"
