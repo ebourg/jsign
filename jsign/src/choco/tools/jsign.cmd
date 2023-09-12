ef4c0b8e08adc5a9045d14cf65cc7acee77af68d
Emmanuel Bourg <ebourg@apache.org>
2023-09-12 15:48:00 +0200

    Support the JSIGN_OPTS environment variable to pass JVM arguments

--- jsign/src/choco/tools/jsign.cmd
+++ jsign/src/choco/tools/jsign.cmd
@@ -1,6 +1,7 @@
 @echo off
 
-java -Djava.net.useSystemProxies=true ^
+java %JSIGN_OPTS% ^
+     -Djava.net.useSystemProxies=true ^
      -Dbasename=jsign ^
      -Dlog4j2.loggerContextFactory=net.jsign.log4j.simple.SimpleLoggerContextFactory ^
      -jar %~dp0\jsign.jar %*
--- jsign/src/deb/data/usr/share/jsign/jsign.sh
+++ jsign/src/deb/data/usr/share/jsign/jsign.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-java -Djava.net.useSystemProxies=true \
+java  $JSIGN_OPTS \
+     -Djava.net.useSystemProxies=true \
      -Dbasename=`basename "$0"` \
      -Dlog4j2.loggerContextFactory=net.jsign.log4j.simple.SimpleLoggerContextFactory \
      -jar /usr/share/jsign/jsign-@VERSION@.jar "$@"
