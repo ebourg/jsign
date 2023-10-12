2892d42b4f32ec9a90a9acc3a69c3a09a65503f1
Emmanuel Bourg <ebourg@apache.org>
2023-10-12 09:53:40 +0200

    Provider.Service implementation using a lambda expression to create the service instances

--- jsign-core/src/main/java/net/jsign/jca/SigningServiceJcaProvider.java
+++ jsign-core/src/main/java/net/jsign/jca/SigningServiceJcaProvider.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2021 Emmanuel Bourg
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +19,6 @@ package net.jsign.jca;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.security.Provider;
-import java.util.Collections;
 
 import net.jsign.DigestAlgorithm;
 
@@ -36,48 +35,21 @@ import net.jsign.DigestAlgorithm;
  */
 public class SigningServiceJcaProvider extends Provider {
 
-    private final SigningService service;
-
     public SigningServiceJcaProvider(SigningService service) {
         super(service.getName(), 1.0, service.getName() + " signing service provider");
-        this.service = service;
 
         AccessController.doPrivileged((PrivilegedAction<Object>) () -> {
-            putService(new KeyStoreProviderService());
+            putService(new ProviderService(this, "KeyStore", service.getName().toUpperCase(), SigningServiceKeyStore.class.getName(), () -> new SigningServiceKeyStore(service)));
+
             for (String alg : new String[]{"RSA", "ECDSA"}) {
                 for (DigestAlgorithm digest : DigestAlgorithm.values()) {
                     if (digest != DigestAlgorithm.MD5) {
-                        putService(new SignatureProviderService(digest.name() + "with" + alg));
+                        String algorithm = digest.name() + "with" + alg;
+                        putService(new ProviderService(this, "Signature", algorithm, SigningServiceSignature.class.getName(), () -> new SigningServiceSignature(algorithm)));
                     }
                 }
             }
             return null;
         });
-    }
-
-    private class KeyStoreProviderService extends Service {
-        public KeyStoreProviderService() {
-            super(SigningServiceJcaProvider.this, "KeyStore", service.getName().toUpperCase(), SigningServiceKeyStore.class.getName(), Collections.emptyList(), null);
-        }
-
-        @Override
-        public Object newInstance(Object constructorParameter) {
-            return new SigningServiceKeyStore(service);
-        }
-    }
-
-    private class SignatureProviderService extends Service {
-
-        private final String signingAlgorithm;
-
-        public SignatureProviderService(String signingAlgorithm) {
-            super(SigningServiceJcaProvider.this, "Signature", signingAlgorithm, SigningServiceSignature.class.getName(), Collections.emptyList(), Collections.emptyMap());
-            this.signingAlgorithm = signingAlgorithm;
-        }
-
-        @Override
-        public Object newInstance(Object constructorParameter) {
-            return new SigningServiceSignature(service, signingAlgorithm);
-        }
     }
 }
