5259dba249c7a4d6611292e638bfe949854eccec
Pascal Davoust <pascal.davoust@enghouse.com>
2024-03-01 16:06:56 +0100

    Fix the log4j configuration warning displayed when signing a MSI file (Fixes #148)

--- jsign/src/choco/tools/jsign.cmd
+++ jsign/src/choco/tools/jsign.cmd
@@ -3,5 +3,4 @@
 java %JSIGN_OPTS% ^
      -Djava.net.useSystemProxies=true ^
      -Dbasename=jsign ^
-     -Dlog4j2.loggerContextFactory=net.jsign.log4j.simple.SimpleLoggerContextFactory ^
      -jar %~dp0\jsign.jar %*
