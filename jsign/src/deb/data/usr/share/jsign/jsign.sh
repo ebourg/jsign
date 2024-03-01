99c159b0764caeeb09cc620dc0155df329e5ca66
Pascal Davoust <pascal.davoust@enghouse.com>
2024-03-01 13:19:38 +0100

    Fix log4j stderr message about not finding a logging implementation when using jsign CLI

--- jsign/src/deb/data/usr/share/jsign/jsign.sh
+++ jsign/src/deb/data/usr/share/jsign/jsign.sh
@@ -3,5 +3,4 @@
 java  $JSIGN_OPTS \
      -Djava.net.useSystemProxies=true \
      -Dbasename=`basename "$0"` \
-     -Dlog4j2.loggerContextFactory=net.jsign.log4j.simple.SimpleLoggerContextFactory \
      -jar /usr/share/jsign/jsign-@VERSION@.jar "$@"
